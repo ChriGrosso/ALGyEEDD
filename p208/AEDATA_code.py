cc7bcd003d2cfef8c00c3237cf2d519ddfea2cf7
ivansuitoo <ivansanchezbona@gmail.com>
2024-10-14 15:44:23 +0200

    Terminado el I.B

--- p208/AEDATA_code.py
+++ p208/AEDATA_code.py
@@ -36,10 +36,43 @@ lst = [ -1, -1, 0, 0, 1, 4, 1, 4]
 print(ds_find(lst, 7))    
 
 
-
+    
 def connected(n,e):
     for n1, n2 in e:
-        if n1 > n or n1 < 0:
+        if n1 > n or n1 <= 0:
             return None
-        if n2 > n or n2 < 0:
+        if n2 > n or n2 <= 0:
             return None
+        
+    p_ds=ds_init(n)
+    for n1, n2 in e: 
+        ds_union(p_ds,n1,n2)
+
+    return p_ds
+
+def connected_count(p):
+    count=0
+    for x in p:
+        if x<0:
+            count=count+1
+    
+    return count
+
+def connected_sets(p_ds):
+    sets = {}
+    
+    # Para cada nodo, encontramos su raíz y lo añadimos al conjunto correspondiente
+    for i in range(len(p_ds)):
+        root = ds_find(p_ds, i)
+        if root not in sets:
+            sets[root] = []
+        sets[root].append(i)
+    
+    # Convertimos el diccionario en una lista de listas
+    return list(sets.values())
+
+lst = [ (1, 4), (3, 4), (2, 5)]
+s = connected(6, lst)
+n = connected_count(s)
+ccp = connected_sets(s)
+print(ccp)
